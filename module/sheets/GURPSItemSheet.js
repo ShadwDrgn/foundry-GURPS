446eda106621d154588b859885865acc2ef50327
ShadwDrgn <c.arcane1@gmail.com>
2020-11-01 20:17:17 -0500

    modifiers on skills items draft

--- module/sheets/GURPSItemSheet.js
+++ module/sheets/GURPSItemSheet.js
@@ -4,8 +4,8 @@ export default class GURPSItemSheet extends ItemSheet {
 			classes: ["gurps", "sheet", "item"]
 		});
 	}
-	//
 	get template() {
+		console.log(this.item);
 		return `systems/gurps/templates/sheets/${this.item.data.type}-sheet.hbs`;
 	}
 	_onDrop(event) {
@@ -15,13 +15,29 @@ export default class GURPSItemSheet extends ItemSheet {
 	      data = JSON.parse(event.dataTransfer.getData('text/plain'));
 	    }
 	    catch (err) {
-	      return false;
+	    	console.log("ERROR");
+	      	return false;
 	    }
-		console.log(data);
+		if (this.item.type == "skill" && game.items.get(data.id).type == "modifier") {
+			if (this.item.data.data.modifiers.includes(data.id) === false) {
+				let curmods = [...this.item.data.data.modifiers];
+				curmods.push(data.id)
+				return this.item.update({"data.modifiers": curmods});
+			}
+		}
 	}
 	activateListeners(html) {
 		super.activateListeners(html);
 		html[0].addEventListener('drop', this._onDrop.bind(this));
-		//html[0].ondrop = this._onDrop.bind(this);
+		//html.find('.gurps.sheet.item form').on("drop", this._onDrop.bind(this));
 	}
+	/** @override */
+	getData() {
+		const data = super.getData();
+		if (this.item.type != "skill") return data;
+		data.mod_items = this.item.data.data.modifiers.map(x => game.items.get(x))
+		console.log(data.mod_items);
+		return data;
+	}
+
 }
